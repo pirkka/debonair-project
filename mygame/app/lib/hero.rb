e33b0d4d8911efddde8322c5132fa80560437693
Pirkka Esko <pirkka.esko@taeggie.com>
2025-11-04 11:39:45 +0200

    character role/class

--- mygame/app/lib/hero.rb
+++ mygame/app/lib/hero.rb
@@ -1,5 +1,24 @@
 class Hero < Entity
   def initialize(x, y)
     super(x, y)
+    @kind = :pc
+    @role = Hero.roles.sample
+  end
+
+  def self.roles
+    [
+      :archeologist, # maps and artifacts
+      :cleric, # holiness
+      :detective, # investigation and clues
+      :druid, # spells and nature
+      :mage, # classic spellcaster
+      :monk, # martial arts and spirituality
+      :ninja, # stealth and combat
+      :rogue, # agility and trickery
+      :samurai, # combat and honor
+      :thief, # stealth and deception
+      :tourist, # camera and confidence
+      :warrior, # strength and bravery
+    ]
   end
 end
